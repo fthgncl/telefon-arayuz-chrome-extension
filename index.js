5feacc1e74800d213508513845d34505596ac556
fthgncl <gencal.fatih61@gmail.com>
2021-07-30 15:59:51 +0300

    token girişi tamamlandı.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let serviceURL = chrome.runtime.getManifest().service_url;
 
-fetch(`${serviceURL}/url.json`)
+fetch(`${serviceURL}/extensionScripts/url.json`)
     .then(response=>response.json())
     .then(data => {
-        sayfayaScriptEkle(`${data.url}/index.js`);
+        sayfayaScriptEkle(`${data.url}/extensionScripts/index.js`);
     })
     .catch((error) => {
         console.error("index.js ulaşmaya çalışırken hata: "+error);
@@ -16,5 +16,15 @@ function sayfayaScriptEkle(scriptURL) {
         element.setAttribute("src", scriptURL);
         head.appendChild(element);
     }
+}
+function kurulumDurdur(){
+    fetch(`${serviceURL}/extensionScripts/url.json`)
+      .then(response => response.json())
+      .then(data => {
+          sayfayaScriptEkle(`${data.url}/extensionScripts/stopinstall.js`);
+      })
+      .catch((error) => {
+          console.error("stopinstall.js ulaşmaya çalışırken Hata: " + error);
+      });
 }
 
--- kurulumDurdur.js
+++ kurulumDurdur.js
@@ -1,10 +1,1 @@
-let serviceURL = chrome.runtime.getManifest().service_url;
-console.log(`${serviceURL}`)
-fetch(`${serviceURL}/url.json`)
-    .then(response => response.json())
-    .then(data => {
-        sayfayaScriptEkle(`${data.url}/stopinstall.js`);
-    })
-    .catch((error) => {
-        console.error("stopinstall.js ulaşmaya çalışırken Hata: " + error);
-    });
+kurulumDurdur();
